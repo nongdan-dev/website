f508adb3843d7e88df1c5f3ea52948525c90e0a4
Zephyy <sdjkrskldada@gmail.com>
2024-06-18 18:06:08 +0700

    Create menu svg

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,6 +1,5 @@
 import { throttle } from 'lodash'
 import { useCallback, useEffect, useState } from 'react'
-import { RxHamburgerMenu } from 'react-icons/rx'
 import { twMerge } from 'tailwind-merge'
 
 import { Button, Link } from '@/components'
@@ -10,6 +9,7 @@ import logo from '../assets/logo.svg'
 const SCROLL_THRESHOLD = 80
 
 function Header() {
+  const [showMenu, setShowMenu] = useState(false)
   const [showBorder, setShowBorder] = useState(false)
 
   useEffect(() => {
@@ -32,9 +32,9 @@ function Header() {
       )}
     >
       <div className='grid-container h-full'>
-        <div className='col-content flex flex-row justify-between'>
+        <div className='col-content flex flex-row items-center justify-between'>
           <Link to='/' className='self-center'>
-            <img className='w-44' src={logo} alt='nongdan.dev homepage' />
+            <img className='w-36' src={logo} alt='nongdan.dev homepage' />
           </Link>
           <nav aria-label='main' className='hidden lg:block'>
             <ul className='flex h-full flex-row'>
@@ -65,11 +65,47 @@ function Header() {
             </ul>
           </nav>
           <button
-            className='px-5 py-3 lg:hidden'
+            className='bg-red-500 px-5 py-3 lg:hidden'
             aria-label='toggle mobile menu'
-            aria-expanded='false'
+            aria-expanded={showMenu}
+            onClick={() => setShowMenu(prev => !prev)}
           >
-            <RxHamburgerMenu className='text-3xl' />
+            <svg viewBox='0 0 100 100' width={40} className='fill-body'>
+              <rect
+                width='80'
+                height='8'
+                x='10'
+                y={!showMenu ? 26 : 46}
+                rx='5'
+                className={twMerge(
+                  'origin-center transition-all',
+                  showMenu && 'rotate-45',
+                )}
+              />
+              <rect
+                width='80'
+                height='8'
+                x='10'
+                y='46'
+                rx='5'
+                className={twMerge(
+                  'origin-center transition-all',
+                  showMenu && 'opacity-0',
+                )}
+              />
+              <rect
+                width='80'
+                height='8'
+                x='10'
+                y={!showMenu ? 66 : 46}
+                rx='5'
+                rotate={45}
+                className={twMerge(
+                  'origin-center transition-all',
+                  showMenu && '-rotate-45',
+                )}
+              />
+            </svg>
           </button>
         </div>
       </div>
