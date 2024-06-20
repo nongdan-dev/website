905e0ad70e51a77a3c3d4c6c98795db67f0739c7
Zephyy <sdjkrskldada@gmail.com>
2024-06-20 17:07:35 +0700

    Update section title

--- src/components/widget/Section.tsx
+++ src/components/widget/Section.tsx
@@ -5,13 +5,36 @@ export type SectionProps = Omit<
   ComponentProps<'section'>,
   'title' | 'children'
 > & {
-  title?: ReactNode
+  title?:
+    | ReactNode
+    | (({
+        titleId,
+        className,
+      }: {
+        titleId: string
+        className: string
+      }) => ReactNode)
   subtitle?: string
   children?: ReactNode | ((titleId: string) => ReactNode)
 }
 
 export function Section({ title, subtitle, children, ...props }: SectionProps) {
   const titleId = useId()
+
+  const titleClassName =
+    'mb-8 text-balance text-4xl font-semibold leading-snug sm:text-5xl sm:leading-snug'
+
+  const renderTitle = () => {
+    if (!title) return null
+    if (typeof title === 'function') {
+      return title({ titleId, className: titleClassName })
+    }
+    return (
+      <h2 id={titleId} className={titleClassName}>
+        {title}
+      </h2>
+    )
+  }
 
   return (
     <section
@@ -27,14 +50,7 @@ export function Section({ title, subtitle, children, ...props }: SectionProps) {
           {subtitle}
         </span>
       )}
-      {!!title && (
-        <h2
-          id={titleId}
-          className='mb-8 text-balance text-4xl font-semibold leading-snug sm:text-5xl sm:leading-snug'
-        >
-          {title}
-        </h2>
-      )}
+      {renderTitle()}
       {typeof children === 'function' ? children(titleId) : children}
     </section>
   )
--- src/pages/Contact/index.tsx
+++ src/pages/Contact/index.tsx
@@ -1,13 +1,23 @@
+import { twMerge } from 'tailwind-merge'
+
 import { Section } from '@/components'
 
 function ContactPage() {
   return (
     <div className='col-content'>
-      <Section subtitle='our approach' title='Engagement Model'>
-        <p>
+      <Section
+        subtitle='our approach'
+        title={({ titleId, className }) => (
+          <h1 id={titleId} className={twMerge(className, 'mb-6')}>
+            Engagement Model
+          </h1>
+        )}
+      >
+        <p className='w-full lg:w-6/12'>
           Our methodology drives a strategic process that helps organizations
           identify and design a proper roadmap.
         </p>
+        <div className='mt-24'>asd</div>
       </Section>
     </div>
   )
