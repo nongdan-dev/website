ca7ad3f120eff3d79bc5b5977c9fd2d70b3dd5d2
Zephyy <sdjkrskldada@gmail.com>
2024-07-01 22:22:39 +0700

    Add embed map

--- src/pages/Contact/index.tsx
+++ src/pages/Contact/index.tsx
@@ -224,7 +224,7 @@ function ContactPage() {
               can help.
             </p>
             <p>Our team will get back to you within 24hrs.</p>
-            <div className='mt-12 grid grid-cols-2 gap-24'>
+            <div className='mt-12 grid grid-cols-2 gap-32'>
               <form onSubmit={_handleSubmit(handleSubmit)}>
                 <FormField label='Name' name='name' control={control} required>
                   <Input placeholder='e.g. John Doe' />
@@ -260,7 +260,14 @@ function ContactPage() {
                   <IoMdSend />
                 </Button>
               </form>
-              <div>maps</div>
+              {/* <div>maps</div> */}
+              <iframe
+                title='maps'
+                src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d501725.4184284701!2d106.36555456720427!3d10.755292872920842!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x317529292e8d3dd1%3A0xf15f5aad773c112b!2zVGjDoG5oIHBo4buRIEjhu5MgQ2jDrSBNaW5oLCBI4buTIENow60gTWluaCwgVmnhu4d0IE5hbQ!5e0!3m2!1svi!2s!4v1719846597084!5m2!1svi!2s'
+                loading='lazy'
+                className='h-full w-full overflow-clip rounded-md'
+                referrerPolicy='no-referrer-when-downgrade'
+              />
             </div>
           </div>
         )}
