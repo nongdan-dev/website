72c90ef1294756b2f040f92baf57d631e85e5263
Zephyy <sdjkrskldada@gmail.com>
2024-07-01 22:26:21 +0700

    Update

--- src/pages/Contact/index.tsx
+++ src/pages/Contact/index.tsx
@@ -260,7 +260,6 @@ function ContactPage() {
                   <IoMdSend />
                 </Button>
               </form>
-              {/* <div>maps</div> */}
               <iframe
                 title='maps'
                 src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d501725.4184284701!2d106.36555456720427!3d10.755292872920842!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x317529292e8d3dd1%3A0xf15f5aad773c112b!2zVGjDoG5oIHBo4buRIEjhu5MgQ2jDrSBNaW5oLCBI4buTIENow60gTWluaCwgVmnhu4d0IE5hbQ!5e0!3m2!1svi!2s!4v1719846597084!5m2!1svi!2s'
