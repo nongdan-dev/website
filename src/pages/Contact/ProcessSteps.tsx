fa9f6b532f3774ca42c3e791868058892e2a562f
Zephyy <sdjkrskldada@gmail.com>
2024-07-02 20:39:12 +0700

    Refactor

---
+++ src/pages/Contact/ProcessSteps.tsx
@@ -0,0 +1,79 @@
+const STEPS = [
+  {
+    id: 1,
+    title: 'Discovery Stage',
+    content: [
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate impedit illo nemo sit similique qui temporibus itaque',
+      'Lorem ipsum dolor sit amet consectetur',
+    ],
+  },
+  {
+    id: 2,
+    title: 'Analysis & Planning',
+    content: [
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates, quis.',
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, dicta.',
+    ],
+  },
+  {
+    id: 3,
+    title: 'UI / UX Design',
+    content: [
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit Voluptatem molestiae maxime quod dicta iste. Reprehenderit quia placeat obcaecati repellat id!',
+    ],
+  },
+  {
+    id: 4,
+    title: 'App Development',
+    content: [
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem molestiae maxime quod dicta iste. Reprehenderit quia',
+      'Lorem ipsum dolor sit adipisicing elit.',
+    ],
+  },
+  {
+    id: 5,
+    title: 'QA Testing',
+    content: [
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem molestiae maxime quod dicta iste. Reprehenderit quia',
+      'Lorem ipsum dolor sit adipisicing elit.',
+    ],
+  },
+  {
+    id: 6,
+    title: 'Deployment & Support',
+    content: [
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem molestiae maxime quod dicta iste. Reprehenderit quia',
+      'Lorem ipsum dolor sit amet elit.',
+    ],
+  },
+]
+
+function ProcessSteps() {
+  return (
+    <div className='mt-24 grid grid-cols-1 gap-10 lg:grid-cols-3'>
+      {STEPS.map(({ id, title, content }) => (
+        <div key={id} className='rounded-md border border-gray-300 p-6'>
+          <span
+            aria-hidden='true'
+            className='mb-2 block text-sm font-bold uppercase text-indigo-500'
+          >
+            STEP {id}
+          </span>
+          <h2
+            aria-label={`Step ${id}. ${title}`}
+            className='mb-4 text-2xl font-medium'
+          >
+            {title}
+          </h2>
+          <div className='flex flex-col gap-1'>
+            {content.map((c, idx) => (
+              <p key={idx}>{c}</p>
+            ))}
+          </div>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default ProcessSteps
--- src/pages/Contact/index.tsx
+++ src/pages/Contact/index.tsx
@@ -4,6 +4,7 @@ import { twMerge } from 'tailwind-merge'
 import { Section } from '@/components'
 
 import ContactForm from './ContactForm'
+import ProcessSteps from './ProcessSteps'
 
 function ContactPage() {
   return (
@@ -20,137 +21,7 @@ function ContactPage() {
           Our methodology drives a strategic process that helps organizations
           identify and design a proper roadmap.
         </p>
-        <div className='mt-24 grid grid-cols-1 gap-10 lg:grid-cols-3'>
-          <div className='rounded-md border border-gray-300 p-6'>
-            <span
-              aria-hidden='true'
-              className='mb-2 block text-sm font-bold uppercase text-indigo-500'
-            >
-              STEP 1
-            </span>
-            <h2
-              aria-label='Step 1. Discovery Stage'
-              className='mb-4 text-2xl font-medium'
-            >
-              Discovery Stage
-            </h2>
-            <div className='flex flex-col gap-1'>
-              <p>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                Voluptate impedit illo nemo sit similique qui temporibus itaque
-              </p>
-              <p>Lorem ipsum dolor sit amet consectetur</p>
-            </div>
-          </div>
-          <div className='rounded-md border border-gray-300 p-6'>
-            <span
-              aria-hidden='true'
-              className='mb-2 block text-sm font-bold uppercase text-indigo-500'
-            >
-              STEP 2
-            </span>
-            <h2
-              aria-label='Step 2. Analysis & Planning'
-              className='mb-4 text-2xl font-medium'
-            >
-              Analysis & Planning
-            </h2>
-            <div className='flex flex-col gap-1'>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Voluptates, quis.
-              </p>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum,
-                dicta.
-              </p>
-            </div>
-          </div>
-          <div className='rounded-md border border-gray-300 p-6'>
-            <span
-              aria-hidden='true'
-              className='mb-2 block text-sm font-bold uppercase text-indigo-500'
-            >
-              STEP 3
-            </span>
-            <h2
-              aria-label='Step 3. UI / UX Design'
-              className='mb-4 text-2xl font-medium'
-            >
-              UI / UX Design
-            </h2>
-            <div className='flex flex-col gap-1'>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Voluptatem molestiae maxime quod dicta iste. Reprehenderit quia
-                placeat obcaecati repellat id!
-              </p>
-            </div>
-          </div>
-          <div className='rounded-md border border-gray-300 p-6'>
-            <span
-              aria-hidden='true'
-              className='mb-2 block text-sm font-bold uppercase text-indigo-500'
-            >
-              STEP 4
-            </span>
-            <h2
-              aria-label='Step 4. App Development'
-              className='mb-4 text-2xl font-medium'
-            >
-              App Development
-            </h2>
-            <div className='flex flex-col gap-1'>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Voluptatem molestiae maxime quod dicta iste. Reprehenderit quia
-              </p>
-              <p>Lorem ipsum dolor sit adipisicing elit.</p>
-            </div>
-          </div>
-          <div className='rounded-md border border-gray-300 p-6'>
-            <span
-              aria-hidden='true'
-              className='mb-2 block text-sm font-bold uppercase text-indigo-500'
-            >
-              STEP 5
-            </span>
-            <h2
-              aria-label='Step 5. QA Testing'
-              className='mb-4 text-2xl font-medium'
-            >
-              QA Testing
-            </h2>
-            <div className='flex flex-col gap-1'>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Voluptatem molestiae maxime quod dicta iste. Reprehenderit quia
-              </p>
-              <p>Lorem ipsum dolor sit adipisicing elit.</p>
-            </div>
-          </div>
-          <div className='rounded-md border border-gray-300 p-6'>
-            <span
-              aria-hidden='true'
-              className='mb-2 block text-sm font-bold uppercase text-indigo-500'
-            >
-              STEP 6
-            </span>
-            <h2
-              aria-label='Step 6. Deployment & Support'
-              className='mb-4 text-2xl font-medium'
-            >
-              Deployment & Support
-            </h2>
-            <div className='flex flex-col gap-1'>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Voluptatem molestiae maxime quod dicta iste. Reprehenderit quia
-              </p>
-              <p>Lorem ipsum dolor sit amet elit.</p>
-            </div>
-          </div>
-        </div>
+        <ProcessSteps />
       </Section>
       <Section className='col-full-width bg-gray-100'>
         {({ titleId }) => (
