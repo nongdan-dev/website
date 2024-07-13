bca0e4216c97ce9d7a58e224ce656df1b3ff894c
Zephyy <sdjkrskldada@gmail.com>
2024-07-13 19:45:38 +0700

    Update our work

--- src/pages/OurWork/index.tsx
+++ src/pages/OurWork/index.tsx
@@ -1,5 +1,22 @@
+import { Fragment } from 'react'
+
+import { LineBreak, Section } from '@/components'
+
 function OurWorkPage() {
-  return <div>work</div>
+  return (
+    <Fragment>
+      <Section
+        subtitle='our work'
+        title={
+          <Fragment>
+            Successful outcomes <LineBreak /> we are proud to be a part of
+          </Fragment>
+        }
+      >
+        <div className='mt-16'>asd</div>
+      </Section>
+    </Fragment>
+  )
 }
 
 export default OurWorkPage
