514f6d227ce8a3ebd644fd18ae83b6c0bb574f8e
Zephyy <sdjkrskldada@gmail.com>
2024-07-15 14:55:56 +0700

    Update our work

--- src/pages/OurWork/index.tsx
+++ src/pages/OurWork/index.tsx
@@ -1,6 +1,6 @@
 import { Fragment } from 'react'
 
-import { LineBreak, Section } from '@/components'
+import { BuildWithUs, LineBreak, Section } from '@/components'
 
 function OurWorkPage() {
   return (
@@ -13,8 +13,159 @@ function OurWorkPage() {
           </Fragment>
         }
       >
-        <div className='mt-16'>asd</div>
+        <div className='mt-20'>
+          <div className='flex gap-8'>
+            <img src='https://placehold.co/500x300' alt='app screenshot' />
+            <div>
+              <div className='mb-4 text-2xl'>APP LOGO</div>
+              <div className='flex flex-col gap-2'>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Obcaecati repellendus, vitae, ea numquam animi saepe ducimus
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </p>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure,
+                  deserunt! Iure dolore, beatae asperiores illum nulla iusto
+                  autem, voluptatum facere voluptate a, ex vel eius consequatur
+                  quasi. Numquam hic accusantium sunt! Vero impedit qui at
+                  quibusdam illum dolore. Labore quia quas ratione animi ab
+                  exercitationem molestiae non totam consequuntur beatae eius
+                  quibusdam amet rem quis nisi sit debitis minima obcaecati
+                  alias tempore, recusandae doloribus ipsam magnam? Similique
+                  quas deserunt earum hic voluptatibus sunt voluptates illo
+                  possimus ratione! Tempora quo delectus ad nesciunt quasi
+                  quisquam inventore fugit porro, veniam aut aliquam error
+                  voluptas sit earum corrupti, magni dolores provident fuga.
+                  Saepe.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className='mt-16 flex gap-8'>
+            <img src='https://placehold.co/500x300' alt='app screenshot' />
+            <div className='order-[-1]'>
+              <div className='mb-4 text-2xl'>APP LOGO</div>
+              <div className='flex flex-col gap-2'>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Obcaecati repellendus, vitae, ea numquam animi saepe ducimus
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </p>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure,
+                  deserunt! Iure dolore, beatae asperiores illum nulla iusto
+                  autem, voluptatum facere voluptate a, ex vel eius consequatur
+                  quasi. Numquam hic accusantium sunt! Vero impedit qui at
+                  quibusdam illum dolore. Labore quia quas ratione animi ab
+                  exercitationem molestiae non totam consequuntur beatae eius
+                  quibusdam amet rem quis nisi sit debitis minima obcaecati
+                  alias tempore, recusandae doloribus ipsam magnam? Similique
+                  quas deserunt earum hic voluptatibus sunt voluptates illo
+                  possimus ratione! Tempora quo delectus ad nesciunt quasi
+                  quisquam inventore fugit porro, veniam aut aliquam error
+                  voluptas sit earum corrupti, magni dolores provident fuga.
+                  Saepe.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className='my-16 h-px w-full bg-gray-200' />
+          <ul className='grid grid-cols-3 gap-16'>
+            <li>
+              <div className='mb-4 text-2xl'>APP LOGO</div>
+              <div className='flex flex-col gap-2'>
+                <p>
+                  Obcaecati repellendus, vitae, ea numquam animi saepe ducimus
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </p>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure,
+                  deserunt! Iure dolore, beatae asperiores illum nulla iusto
+                  autem, voluptatum facere voluptate a, ex vel eius consequatur
+                  quasi. Numquam hic accusantium sunt! Vero impedit qui at
+                  quibusdam illum dolore. Labore quia quas ratione animi ab
+                  exercitationem molestiae non totam consequuntur beatae eius
+                  quibusdam amet rem quis nisi sit debitis minima obcaecati
+                </p>
+              </div>
+            </li>
+            <li>
+              <div className='mb-4 text-2xl'>APP LOGO</div>
+              <div className='flex flex-col gap-2'>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Obcaecati repellendus, vitae, ea numquam animi saepe ducimus
+                </p>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure,
+                  deserunt! Iure dolore, beatae asperiores illum nulla iusto
+                  autem, voluptatum facere voluptate a, ex vel eius consequatur
+                  quasi. Numquam hic accusantium sunt! Vero impedit qui at
+                  quibusdam illum dolore. Labore quia quas ratione animi ab
+                  exercitationem molestiae non totam consequuntur beatae eius
+                  Saepe.
+                </p>
+              </div>
+            </li>
+            <li>
+              <div className='mb-4 text-2xl'>APP LOGO</div>
+              <div className='flex flex-col gap-2'>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Obcaecati repellendus, vitae, ea numquam animi saepe ducimus
+                </p>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure,
+                  deserunt! Iure dolore, beatae asperiores illum nulla iusto
+                  autem, voluptatum facere voluptate a, ex vel eius consequatur
+                  quasi. Numquam hic accusantium sunt! Vero impedit qui at
+                  quibusdam illum dolore. Labore quia quas ratione animi ab
+                  exercitationem molestiae non totam consequuntur beatae eius
+                  Saepe.
+                </p>
+              </div>
+            </li>
+            <li>
+              <div className='mb-4 text-2xl'>APP LOGO</div>
+              <div className='flex flex-col gap-2'>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Obcaecati repellendus, vitae, ea numquam animi saepe ducimus
+                </p>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure,
+                  deserunt! Iure dolore, beatae asperiores illum nulla iusto
+                  autem, voluptatum facere voluptate a, ex vel eius consequatur
+                  quasi. Numquam hic accusantium sunt! Vero impedit qui at
+                  quibusdam illum dolore. Labore quia quas ratione animi ab
+                  exercitationem molestiae non totam consequuntur beatae eius
+                  Saepe.
+                </p>
+              </div>
+            </li>
+            <li>
+              <div className='mb-4 text-2xl'>APP LOGO</div>
+              <div className='flex flex-col gap-2'>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Obcaecati repellendus, vitae, ea numquam animi saepe ducimus
+                </p>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure,
+                  deserunt! Iure dolore, beatae asperiores illum nulla iusto
+                  autem, voluptatum facere voluptate a, ex vel eius consequatur
+                  quasi. Numquam hic accusantium sunt! Vero impedit qui at
+                  quibusdam illum dolore. Labore quia quas ratione animi ab
+                  exercitationem molestiae non totam consequuntur beatae eius
+                  Saepe.
+                </p>
+              </div>
+            </li>
+          </ul>
+        </div>
       </Section>
+      <BuildWithUs />
     </Fragment>
   )
 }
