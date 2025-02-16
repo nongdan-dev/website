70aa36c919f9b34a38fe9e33044a5844ab752b38
Zephyy <sdjkrskldada@gmail.com>
2025-02-16 21:24:44 +0700

    add clients section

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,13 @@
 import { Fragment } from 'react'
 
+import Clients from './Clients'
 import Hero from './Hero'
 
 function HomePage() {
   return (
     <Fragment>
       <Hero />
+      <Clients />
     </Fragment>
   )
 }
