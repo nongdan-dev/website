90dc366f454fa0c4998909a9c8fd8af1dc6b1cea
Zephyy <sdjkrskldada@gmail.com>
2025-02-20 20:50:35 +0700

    update

--- src/pages/AboutUs/TeamMember.tsx
+++ src/pages/AboutUs/TeamMember.tsx
@@ -1,57 +1,28 @@
+import { LineBreak } from '@/components/ui'
 import { Section } from '@/components/widget'
-
-const teamMembers = [
-  { name: 'Michael McElhaney', role: 'Founder & CEO', image: '' },
-  { name: 'Howard Rutherford', role: 'Founder & CEO', image: '' },
-  { name: 'Hana Rosum', role: 'Head of Sales', image: '' },
-  {
-    name: 'Durandana Croquetaigne',
-    role: 'Product Manager',
-    image: '',
-  },
-  { name: 'Emily Havanna', role: 'Product Designer', image: '' },
-  { name: 'Alice Carey', role: 'UX Researcher', image: '' },
-  {
-    name: 'Gabriel Vankirk',
-    role: 'Software Engineer',
-    image: '',
-  },
-  { name: 'James Redding', role: 'Software Engineer', image: '' },
-  { name: 'Walter Debose', role: 'DevOps', image: '' },
-  { name: 'Charles Delacruz', role: 'Product Designer', image: '' },
-]
 
 export const TeamMember = () => {
   return (
     <Section>
       <div className='w-full place-items-center text-center'>
-        <Section.Title className='w-full max-w-[56rem]'>
-          Meet our team of <span className='text-blue-600'>creators</span>,
-          <span className='text-blue-600'> designers</span> and
-          <span className='text-blue-600'> problem solvers</span>
+        <Section.Title className='w-4/5'>
+          Meet our team of <span className='text-primary-500'>creators</span>,
+          <LineBreak />
+          <span className='text-primary-500'> designers</span> and
+          <span className='text-primary-500'> problem solvers</span>
         </Section.Title>
       </div>
-      <div className='mt-12 grid grid-cols-2 gap-8 md:grid-cols-3 lg:grid-cols-4'>
-        {teamMembers.map((member, index) => (
-          <div
-            key={index}
-            className='flex w-full flex-col items-center rounded-lg bg-white text-center'
-          >
-            <div className='h-80 w-full bg-gray-400'>
-              <img src={member.image} alt='' />
-            </div>
-
-            <div className='mt-6 flex w-full flex-col items-start'>
-              <p className='mb-1.5 text-xl font-medium leading-none text-gray-900'>
-                {member.name}
-              </p>
-              <p className='text-base font-normal leading-none text-gray-500'>
-                {member.role}
-              </p>
+      <Section.Content className='grid grid-cols-4 gap-8'>
+        {Array.from({ length: 12 }).map((_, idx) => (
+          <div key={idx} className='flex w-full flex-col items-center gap-6'>
+            <div className='h-[300px] w-full rounded-xl border border-gray-200 bg-gray-200 shadow-sm'></div>
+            <div className='flex w-full flex-col gap-0.5'>
+              <p className='text-xl font-medium'>Alan Runner</p>
+              <p className='text-gray-500'>Software Engineer</p>
             </div>
           </div>
         ))}
-      </div>
+      </Section.Content>
     </Section>
   )
 }
