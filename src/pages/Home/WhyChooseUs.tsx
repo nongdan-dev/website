34b651256e0ce5e0590a7c1be3852a018ac1131e
Zephyy <sdjkrskldada@gmail.com>
2025-02-23 17:19:07 +0700

    responsive grid layout

--- src/pages/Home/WhyChooseUs.tsx
+++ src/pages/Home/WhyChooseUs.tsx
@@ -13,59 +13,77 @@ function WhyChooseUs() {
         <Section.Title>The right tech partner for your growth</Section.Title>
       </div>
       <Section.Content id='grid-layout'>
-        <div className='rounded-xl border border-gray-200 p-8 shadow-sm'>
-          <img src={WhyChooseUs1} alt='' className='w-full' />
+        <div className='place-content-center rounded-xl border border-gray-200 p-8 shadow-sm'>
+          <img
+            src={WhyChooseUs1}
+            alt=''
+            className='place-self-center md:w-3/4 xl:w-full'
+          />
           <div className='mt-8 flex flex-col gap-2.5 text-center'>
-            <h3 className='text-3xl font-medium'>Modern Technology</h3>
-            <p className='mx-auto w-10/12 text-center text-xl'>
+            <h3 className='text-2xl font-medium md:text-3xl'>
+              Modern Technology
+            </h3>
+            <p className='mx-auto w-10/12 text-center sm:text-xl'>
               Leveraging the latest tech for fast, secure, and scalable
               applications.
             </p>
           </div>
         </div>
-        <div className='flex flex-col justify-between rounded-xl border border-gray-200 p-8 shadow-sm'>
+        <div className='flex flex-col justify-between gap-8 rounded-xl border border-gray-200 p-8 shadow-sm'>
           <CheckCircle className='text-4xl text-primary-500' />
           <div className='flex flex-col gap-3'>
-            <h3 className='text-3xl font-medium'>Custom Solutions</h3>
-            <p className='text-xl'>
+            <h3 className='text-2xl font-medium md:text-3xl'>
+              Custom Solutions
+            </h3>
+            <p className='sm:text-xl'>
               We develop software tailored to your specific needs, not generic
               templates.
             </p>
           </div>
         </div>
-        <div className='flex flex-col justify-between rounded-xl border border-gray-200 p-8 shadow-sm'>
+        <div className='flex flex-col justify-between gap-8 rounded-xl border border-gray-200 p-8 shadow-sm'>
           <CheckCircle className='text-4xl text-primary-500' />
           <div className='flex flex-col gap-3'>
-            <h3 className='text-3xl font-medium'>Flexible Integrations</h3>
-            <p className='text-xl'>
+            <h3 className='text-2xl font-medium md:text-3xl'>
+              Flexible Integrations
+            </h3>
+            <p className='sm:text-xl'>
               Seamless connection with payment systems, CRM, AI, and more.
             </p>
           </div>
         </div>
-        <div className='flex flex-col justify-between rounded-xl border border-gray-200 p-8 shadow-sm'>
+        <div className='flex flex-col justify-between gap-8 rounded-xl border border-gray-200 p-8 shadow-sm'>
           <CheckCircle className='text-4xl text-primary-500' />
           <div className='flex flex-col gap-3'>
-            <h3 className='text-3xl font-medium'>High Security</h3>
-            <p className='text-xl'>
+            <h3 className='text-2xl font-medium md:text-3xl'>High Security</h3>
+            <p className='sm:text-xl'>
               Implementing top security standards to protect your data and
               privacy.
             </p>
           </div>
         </div>
-        <div className='flex flex-col justify-between rounded-xl border border-gray-200 p-8 shadow-sm'>
+        <div className='flex flex-col justify-between gap-8 rounded-xl border border-gray-200 p-8 shadow-sm'>
           <CheckCircle className='text-4xl text-primary-500' />
           <div className='flex flex-col gap-3'>
-            <h3 className='text-3xl font-medium'>Long-term Support</h3>
-            <p className='text-xl'>
+            <h3 className='text-2xl font-medium md:text-3xl'>
+              Long-term Support
+            </h3>
+            <p className='sm:text-xl'>
               Beyond development, we offer maintenance, updates, and upgrades.
             </p>
           </div>
         </div>
-        <div className='rounded-xl border border-gray-200 p-8 shadow-sm'>
-          <img src={WhyChooseUs2} alt='' className='w-full' />
+        <div className='place-content-center rounded-xl border border-gray-200 p-8 shadow-sm'>
+          <img
+            src={WhyChooseUs2}
+            alt=''
+            className='place-self-center md:w-3/4 xl:w-full'
+          />
           <div className='mt-8 flex flex-col gap-2.5 text-center'>
-            <h3 className='text-3xl font-medium'>Fast Deployment</h3>
-            <p className='mx-auto w-10/12 text-center text-xl'>
+            <h3 className='text-2xl font-medium md:text-3xl'>
+              Fast Deployment
+            </h3>
+            <p className='mx-auto w-10/12 text-center sm:text-xl'>
               Streamlined processes ensure quick go-to-market without
               compromising quality.
             </p>
