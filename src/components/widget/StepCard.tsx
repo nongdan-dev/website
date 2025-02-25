8c78f9567d2687f472251edf7c3b244bbae7f599
JavascriptRepo <dinhhuuthaian9191@gmail.com>
2025-02-25 14:52:52 +0700

    delete hover

--- src/components/widget/StepCard.tsx
+++ src/components/widget/StepCard.tsx
@@ -16,7 +16,7 @@ const StepCard: React.FC<StepCardProps> = ({
   return (
     <div className='group relative flex flex-col items-center justify-center'>
       {index !== 0 && <div className='flex h-[45px] w-[2px] bg-gray-100'></div>}
-      <div className='grid gap-2 rounded-lg border border-gray-200 p-6 shadow-sm hover:shadow-md'>
+      <div className='grid gap-2 rounded-lg border border-gray-200 p-6 shadow-sm'>
         <div className='flex items-center gap-3'>
           <div>{icon}</div>
           <h3 className='text-lg font-semibold'>{title}</h3>
