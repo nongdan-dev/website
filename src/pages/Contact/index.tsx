ca9cf20dada1e179ebcec7074d411e4baddf8bc4
Zephyy <sdjkrskldada@gmail.com>
2025-02-28 19:46:55 +0700

    update map

--- src/pages/Contact/index.tsx
+++ src/pages/Contact/index.tsx
@@ -21,8 +21,9 @@ export default function ContactUs() {
       </p>
       <Section.Content className='grid grid-cols-1 gap-y-10 lg:grid-cols-2'>
         <ContactForm />
+
         <iframe
-          src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4143.1550046811135!2d106.84146337528799!3d10.838145089314422!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752126d447fbd5%3A0xa714c8e67c35f0b0!2sAnnam%20cuisine!5e1!3m2!1svi!2s!4v1740117986089!5m2!1svi!2s'
+          src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2199.2784372697033!2d106.84433090321284!3d10.836657442334598!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752126d447fbd5%3A0xa714c8e67c35f0b0!2sAnnam%20cuisine!5e0!3m2!1sen!2s!4v1740746756579!5m2!1sen!2s'
           allowFullScreen
           loading='lazy'
           title='our HQ location'
