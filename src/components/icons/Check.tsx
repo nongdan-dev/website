02a28410c2fa18c47cd83939a6a1ec00b176b347
Duy Nguyen <duynmps08758@gmail.com>
2025-03-03 15:01:09 +0700

    fix checkbox

---
+++ src/components/icons/Check.tsx
@@ -0,0 +1,21 @@
+import { ComponentProps } from 'react'
+
+export function Check(props: ComponentProps<'svg'>) {
+  return (
+    <svg
+      width='24'
+      height='24'
+      viewBox='0 0 24 24'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+      {...props}
+    >
+      <path
+        d='M20 7L9.35355 17.6464C9.15829 17.8417 8.84171 17.8417 8.64645 17.6464L4 13'
+        stroke='currentColor'
+        stroke-width='2'
+        stroke-linecap='round'
+      />
+    </svg>
+  )
+}
