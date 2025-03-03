34ef8aa61f2cfddddc8778eed163b5f5114c6c43
Duy Nguyen <duynmps08758@gmail.com>
2025-03-03 15:06:07 +0700

    fix error dom

--- src/components/icons/Check.tsx
+++ src/components/icons/Check.tsx
@@ -13,8 +13,8 @@ export function Check(props: ComponentProps<'svg'>) {
       <path
         d='M20 7L9.35355 17.6464C9.15829 17.8417 8.84171 17.8417 8.64645 17.6464L4 13'
         stroke='currentColor'
-        stroke-width='2'
-        stroke-linecap='round'
+        strokeWidth='2'
+        strokeLinecap='round'
       />
     </svg>
   )
