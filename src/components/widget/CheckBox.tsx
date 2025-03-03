97b33a391bb6c0aec01224494fd5c08f69a96934
Duy Nguyen <duynmps08758@gmail.com>
2025-03-03 14:48:36 +0700

    fix review

--- src/components/widget/CheckBox.tsx
+++ src/components/widget/CheckBox.tsx
@@ -1,5 +1,5 @@
+import * as Checkbox from '@radix-ui/react-checkbox'
 import { CheckIcon } from '@radix-ui/react-icons'
-import { Checkbox } from 'radix-ui'
 
 interface CheckBoxProps {
   id: string
@@ -15,7 +15,7 @@ const CheckBox = ({ id, value, checked, onChange }: CheckBoxProps) => {
         id={id}
         checked={checked}
         onCheckedChange={onChange}
-        className='flex h-6 w-6 items-center justify-center rounded-[0.1875rem] border border-gray-300 bg-white hover:border-primary-500 focus:border-primary-500 focus:ring-1 focus:ring-primary-500'
+        className='flex h-6 w-6 items-center justify-center rounded-[3px] border border-gray-300 bg-white hover:border-primary-500 focus:border-primary-500 focus:ring-1 focus:ring-primary-500'
       >
         <Checkbox.Indicator className='text-primary-500'>
           <CheckIcon className='h-4 w-4' />
