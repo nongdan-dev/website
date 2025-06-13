d33bcee55ecb34adfc9451ab5177ba62512fe669
Duy Nguyen <duynmps08758@gmail.com>
2025-06-13 17:28:00 +0700

    kebab

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -6,7 +6,7 @@ import { SectionCTA, SectionAboutUs,SubTitle } from '@/components/widget'
 
 import Clients from './clients'
 import Feedback from './feedback'
-import Hero from './Hero1'
+import Hero from './hero'
 import OurWork from './OurWork'
 import WhatWeOffer from './WhatWeOffer'
 import WhyChooseUs from './WhyChooseUs'
