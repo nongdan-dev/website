2269c8c737b25b09a540c4d3d1afed4ef0c67be4
Duy Nguyen <duynmps08758@gmail.com>
2025-06-26 22:35:31 +0700

    update code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export const metadata: Metadata = {
   title: 'Nongdandev',
   description: 'Technology solutions for modern agriculture',
   icons: {
-    icon: '/favicon.ico',
+    icon: '/favicon-new.ico',
   },
   metadataBase: new URL(
     process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3000',
